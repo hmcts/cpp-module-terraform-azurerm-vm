a5262121ad46a7e43ec5f29b87105276d9681482
varun-vijayagopal <151660146+varun-vijayagopal@users.noreply.github.com>
2023-12-06 08:44:11 +0000

    Ei 1547 (#2)
    * vm module
    
    * vm module
    
    * vm module
    
    * vm module
    
    * vm module
    
    * vm module
    
    * vm module
    
    * vm module
    
    * vm module
    
    * vm module
    
    * vm module
    
    * vm module
    
    * vm module
    
    * vm module
    
    * vm module
    
    * vm module
    
    * vm module
    
    * vm module

---
+++ examples/variables.tf
@@ -0,0 +1,29 @@
+variable "resource_group_name" {
+  type        = string
+  default     = ""
+  description = "description"
+}
+
+variable "location" {
+  type        = string
+  default     = ""
+  description = "description"
+}
+
+variable "subnet_config" {
+  type        = map(string)
+  default     = {}
+  description = "Subnet config"
+}
+
+variable "key_vault_config" {
+  type        = map(string)
+  default     = {}
+  description = "Key vault config"
+}
+
+variable "vmss_config" {
+  type        = map(string)
+  default     = {}
+  description = "VMSS config"
+}
---
+++ tests/terratest/vm_test.go
@@ -0,0 +1,28 @@
+package test
+
+import (
+	"testing"
+    "github.com/stretchr/testify/assert"
+	"github.com/gruntwork-io/terratest/modules/terraform"
+)
+
+func TestTerraformVM(t *testing.T) {
+	t.Parallel()
+
+	terraformOptions := &terraform.Options{
+		// The path to where our Terraform code is located
+		TerraformDir: "../../examples",
+		VarFiles: []string{"terratest.tfvars"},
+		Upgrade: true,
+	}
+
+	// Defer the destroy to cleanup all created resources
+	defer terraform.Destroy(t, terraformOptions)
+
+	// This will init and apply the resources and fail the test if there are any errors
+	terraform.InitAndApply(t, terraformOptions)
+
+	// // Assert inputs with outputs
+	outputs_vm_name := terraform.Output(t, terraformOptions, "linux_virtual_machine_name")
+	assert.Equal(t, "vm-linux", outputs_vm_name)
+}
