80b117f63f54fc5359f8bf06f06e9a782c139647
Chirag Pareek <chirag.pareek@justice.gov.uk>
2025-03-27 16:31:58 +0000

    updated condtion

--- rbac.tf
+++ rbac.tf
@@ -1,12 +1,12 @@
 resource "azurerm_role_assignment" "admin-user" {
-  count                = var.deploy_entra_extension && var.os_flavor == "linux" ? var.instances_count : 0
+  count                = var.deploy_entra_extension && var.os_flavor == "linux" && var.admin_user_group_id != null ? var.instances_count : 0
   scope                = azurerm_linux_virtual_machine.linux_vm[count.index].id
   role_definition_name = "Virtual Machine Administrator Login"
   principal_id         = var.admin_user_group_id
 }
 
 resource "azurerm_role_assignment" "standard-user" {
-  count                = var.deploy_entra_extension && var.os_flavor == "linux" ? var.instances_count : 0
+  count                = var.deploy_entra_extension && var.os_flavor == "linux" && var.standard_user_group_id != null ? var.instances_count : 0
   scope                = azurerm_linux_virtual_machine.linux_vm[count.index].id
   role_definition_name = "Virtual Machine User Login"
   principal_id         = var.standard_user_group_id
