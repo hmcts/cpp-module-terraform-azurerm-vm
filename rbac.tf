23cffd3404f2fc19267b3cbc490a60a1ef7e6076
Chirag Pareek <chirag.pareek@justice.gov.uk>
2025-03-28 12:36:27 +0000

    changed assignment scope

--- rbac.tf
+++ rbac.tf
@@ -1,13 +1,13 @@
 resource "azurerm_role_assignment" "admin-user" {
-  count                = var.deploy_entra_extension && var.os_flavor == "linux" && var.admin_user_group_id != null ? var.instances_count : 0
-  scope                = azurerm_linux_virtual_machine.linux_vm[count.index].id
+  count                = var.deploy_entra_extension && var.os_flavor == "linux" && var.admin_user_group_id != null ? 1 : 0
+  scope                = data.azurerm_resource_group.rg.id
   role_definition_name = "Virtual Machine Administrator Login"
   principal_id         = var.admin_user_group_id
 }
 
 resource "azurerm_role_assignment" "standard-user" {
-  count                = var.deploy_entra_extension && var.os_flavor == "linux" && var.standard_user_group_id != null ? var.instances_count : 0
-  scope                = azurerm_linux_virtual_machine.linux_vm[count.index].id
+  count                = var.deploy_entra_extension && var.os_flavor == "linux" && var.standard_user_group_id != null ? 1 : 0
+  scope                = data.azurerm_resource_group.rg.id
   role_definition_name = "Virtual Machine User Login"
   principal_id         = var.standard_user_group_id
 }
