c0817cf41e6771f35be1a8a29a382386138b138e
Chirag Pareek <chirag.pareek@justice.gov.uk>
2025-05-22 12:47:33 +0100

    added one more change

--- main.tf
+++ main.tf
@@ -204,7 +204,7 @@ resource "azurerm_linux_virtual_machine" "linux_vm" {
   availability_set_id             = var.enable_vm_availability_set == true ? element(concat(azurerm_availability_set.aset.*.id, [""]), 0) : null
   encryption_at_host_enabled      = var.enable_encryption_at_host
   proximity_placement_group_id    = var.enable_proximity_placement_group ? azurerm_proximity_placement_group.appgrp.0.id : null
-  zone                            = var.vm_availability_zone == null ? var.zones_list[count.index] : var.vm_availability_zone
+  zone                            = length(var.zones_list) > 0 ? var.zones_list[count.index] : var.vm_availability_zone
 
   tags = merge(
     {
@@ -298,7 +298,7 @@ resource "azurerm_windows_virtual_machine" "win_vm" {
   encryption_at_host_enabled   = var.enable_encryption_at_host
   proximity_placement_group_id = var.enable_proximity_placement_group ? azurerm_proximity_placement_group.appgrp.0.id : null
   patch_mode                   = var.patch_mode
-  zone                         = var.vm_availability_zone
+  zone                         = length(var.zones_list) > 0 ? var.zones_list[count.index] : var.vm_availability_zone
   timezone                     = var.vm_time_zone
 
   tags = merge(
